746f9e67cc528e4a252c1eb37a79870e86e79374
Artyom Abramyan <artemabramyan7@icloud.com>
2022-10-13 09:54:11 +0300

    Сделал лабу

---
+++ Item.cs
@@ -0,0 +1,18 @@
+﻿namespace ega_lab7;
+
+public struct Item
+{
+	public int Value { get; }
+	public int Weight { get; }
+
+	public Item(int value, int weight)
+	{
+		Value = value;
+		Weight = weight;
+	}
+
+	public override string ToString()
+	{
+		return $"(Value: {Value}, Weight: {Weight})";
+	}
+}
